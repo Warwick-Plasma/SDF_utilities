f2ffe1a677c70946898d29115ec8ca11e1803115
Keith Bennett <K.Bennett@warwick.ac.uk>
2015-05-27 21:56:03 +0100

    Speed up location of numpy header file

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
 import os, sys
 from distutils.sysconfig import get_python_lib
 from distutils.core import setup, Extension
+try:
+  import numpy
+  gotnumpy = True
+except:
+  gotnumpy = False
 
 def get_numpy_dir():
+  if gotnumpy:
+    numpydir = os.path.dirname(numpy.__file__)
+    incdir = os.path.join(numpydir, 'core', 'include')
+    for r,d,fl in os.walk(incdir):
+      for f in fl:
+        if f == 'arrayobject.h':
+          return os.path.realpath(os.path.join(r,'..'))
+    for r,d,fl in os.walk(numpydir):
+      for f in fl:
+        if f == 'arrayobject.h':
+          return os.path.realpath(os.path.join(r,'..'))
   sys.path.insert(0,get_python_lib(standard_lib=1))
   for path in sys.path:
     for r,d,fl in os.walk(path):
