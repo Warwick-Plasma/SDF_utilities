4171ab69251d1c35369ae4e54bc658dd524d74fe
Keith Bennett <K.Bennett@warwick.ac.uk>
2015-06-18 14:45:43 +0100

    Fixed python scripts to be PEP8 compliant

--- setup.py
+++ setup.py
@@ -1,41 +1,44 @@
-import os, sys
+import os
+import sys
 from distutils.sysconfig import get_python_lib
 from distutils.core import setup, Extension
 try:
-  import numpy
-  gotnumpy = True
+    import numpy
+    gotnumpy = True
 except:
-  gotnumpy = False
+    gotnumpy = False
+
 
 def get_numpy_dir():
-  if gotnumpy:
-    numpydir = os.path.dirname(numpy.__file__)
-    incdir = os.path.join(numpydir, 'core', 'include')
-    for r,d,fl in os.walk(incdir):
-      for f in fl:
-        if f == 'arrayobject.h':
-          return os.path.realpath(os.path.join(r,'..'))
-    for r,d,fl in os.walk(numpydir):
-      for f in fl:
-        if f == 'arrayobject.h':
-          return os.path.realpath(os.path.join(r,'..'))
-  sys.path.insert(0,get_python_lib(standard_lib=1))
-  for path in sys.path:
-    for r,d,fl in os.walk(path):
-      for f in fl:
-        if f == 'arrayobject.h':
-          return os.path.realpath(os.path.join(r,'..'))
-  print('Unable to build python module. Numpy directory not found.')
-  sys.exit(1)
+    if gotnumpy:
+        numpydir = os.path.dirname(numpy.__file__)
+        incdir = os.path.join(numpydir, 'core', 'include')
+        for r, d, fl in os.walk(incdir):
+            for f in fl:
+                if f == 'arrayobject.h':
+                    return os.path.realpath(os.path.join(r, '..'))
+        for r, d, fl in os.walk(numpydir):
+            for f in fl:
+                if f == 'arrayobject.h':
+                    return os.path.realpath(os.path.join(r, '..'))
+    sys.path.insert(0, get_python_lib(standard_lib=1))
+    for path in sys.path:
+        for r, d, fl in os.walk(path):
+            for f in fl:
+                if f == 'arrayobject.h':
+                    return os.path.realpath(os.path.join(r, '..'))
+    print('Unable to build python module. Numpy directory not found.')
+    sys.exit(1)
 
 
-sdfdir = os.path.join('..','C')
+sdfdir = os.path.join('..', 'C')
 
 srcfiles = ['sdf_python.c']
 
-incdirs = [get_numpy_dir()] + [os.path.join(sdfdir,'src')]
-libdirs = [os.path.join(sdfdir,'lib')]
+incdirs = [get_numpy_dir()] + [os.path.join(sdfdir, 'src')]
+libdirs = [os.path.join(sdfdir, 'lib')]
 
 setup(name="sdf", version="1.0",
       ext_modules=[Extension("sdf", srcfiles, include_dirs=incdirs,
-      library_dirs=libdirs, libraries=['sdfc'])], py_modules=["sdf_legacy"])
+                   library_dirs=libdirs, libraries=['sdfc'])],
+      py_modules=["sdf_legacy"])
--- test.py
+++ test.py
@@ -1,11 +1,32 @@
-from numpy import *
-from sdf import *
+import sys
+import sdf
 import matplotlib.pyplot as plt
 
-a=SDF("0010.sdf")
-b=a.read()
-fig = plt.figure()
-ax = fig.add_subplot(111)
-plt.plot(b['grid/X'],b['ex'][:,64],'r+')
-plt.show()
+fname = "Data/0010.sdf"
+varname = "Electric_Field_Ex"
 
+try:
+    d = sdf.read(fname)
+except:
+    print 'File "%s" not found' % fname
+    sys.exit()
+
+if not hasattr(d, varname):
+    print 'Variable "%s" not found in file' % varname
+    sys.exit()
+
+var = d.__dict__[varname]
+
+if len(var.dims) != 2:
+    print 'File "%s" is not from a 2D run' % fname
+    sys.exit()
+
+iy = var.dims[1] / 2
+grid = var.grid_mid
+x = grid.data[0]
+
+fig = plt.figure()
+plt.plot(x, var.data[:, iy], 'r+-')
+plt.xlabel(grid.labels[0] + r' $(' + grid.units[0] + ')$')
+plt.ylabel(var.name + r' $(' + var.units + ')$')
+plt.show()
