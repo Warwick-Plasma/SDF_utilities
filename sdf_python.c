a752f6edfd8ead716c89618048206be74882137b
Keith Bennett <K.Bennett@warwick.ac.uk>
2015-06-18 09:56:49 +0100

    Bumped version number

--- sdf_python.c
+++ sdf_python.c
@@ -1491,7 +1491,7 @@ MOD_INIT(sdf)
     if (!m)
         return MOD_ERROR_VAL;
 
-    PyModule_AddStringConstant(m, "__version__", "2.2.0");
+    PyModule_AddStringConstant(m, "__version__", "2.2.1");
 
     SDFType.tp_dealloc = SDF_dealloc;
     SDFType.tp_flags = Py_TPFLAGS_DEFAULT;
