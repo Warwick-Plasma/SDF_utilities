fabe5ace46f8c762a51da81ca90b7737e76debba
Keith Bennett <K.Bennett@warwick.ac.uk>
2015-10-21 12:08:04 +0100

    Bumped version number

--- sdf_python.c
+++ sdf_python.c
@@ -1547,7 +1547,7 @@ MOD_INIT(sdf)
     if (!m)
         return MOD_ERROR_VAL;
 
-    PyModule_AddStringConstant(m, "__version__", "2.4.1");
+    PyModule_AddStringConstant(m, "__version__", "2.4.3");
     PyModule_AddStringConstant(m, "__commit_id__", SDF_COMMIT_ID);
     PyModule_AddStringConstant(m, "__commit_date__", SDF_COMMIT_DATE);
     s = sdf_get_library_commit_id();
